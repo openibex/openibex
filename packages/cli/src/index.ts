e4904ed2716be5073a91ce09191654d43b93c05d
Lukas Meyer <lukas@ibex.host>
2024-11-09 07:19:03 +0100

    #15 - CLI command finishing: App can now import packages on start. Watch has better logging and publishing notes are updated.

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -13,13 +13,25 @@ import InitCommand from './commands/init';
 import ProtocolCommand from './commands/protocol';
 import ConnectCommand from './commands/connect';
 import ExecCommand from './commands/execute';
+import ValueCommand from './commands/value';
+import WatchCommand from './commands/watch';
+import path from 'path';
+import { pathToFileURL } from 'url';
 
 
 
 /**
  * Add new plugins here
  */
-let localCommands = [InitCommand, StartCommand, ProtocolCommand, ConnectCommand, ExecCommand];
+let localCommands = [
+  InitCommand, 
+  StartCommand, 
+  ProtocolCommand, 
+  ConnectCommand, 
+  ExecCommand, 
+  ValueCommand,
+  WatchCommand
+];
 
 let core: OiCore;
 
@@ -32,6 +44,7 @@ async function main() {
     .description('A CLI for OpenIbex applications')
     .allowUnknownOption()
     .version('1.0.0')
+    .option('--import <package>', 'Dynamically import a package from a path before executing commands')
     .option('--config <path>', 'Specify a custom configuration file', 'config.yaml')
     .option('--logdest <path>', 'Specify the log destination file', 'logs/app.log')
     .helpOption('--help', 'Display help for the CLI');
@@ -44,6 +57,23 @@ async function main() {
   const config = await loadConfig(options.config);
   const oiLogger = getOiLogger(options.logdest);
   oiLogger.info('Configuration loaded successfully.');
+
+  // Import package
+  if (options.import) {
+    const importPackage = options.import;
+    try {
+      const packagePath = path.isAbsolute(importPackage)
+        ? pathToFileURL(importPackage)
+        : pathToFileURL(path.resolve(process.cwd(), importPackage));
+      
+      // Dynamically import the package
+      import(packagePath.toString())
+        .then((pkg) => oiLogger.info(`Successfully imported package: ${importPackage}`))
+        .catch((err) => oiLogger.error(`Failed to import package "${importPackage}": ${err.message}`));
+    } catch (error) {
+      oiLogger.error(`Error importing package: ${error.message}`);
+    }
+  }
 
   try {
     core = await getOiCore(config, oiLogger);
