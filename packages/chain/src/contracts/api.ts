d760d2b08e6b8a46265e8f9cdb32139e400cd6c5
Thomas Bergmueller <thomas.bergmueller@authenticvision.com>
2025-01-10 19:37:09 +0100

    chain/execute: Support struct arguments
    - Allow JSON-Syntax in args in order to provide struct arguments.
      Supports Nested JSON
    - Support exec for pure functions

--- packages/chain/src/contracts/api.ts
+++ packages/chain/src/contracts/api.ts
@@ -33,52 +33,105 @@ export class OiContractAPI {
     return this.contract;
   }
 
-  private async prepareCall(functionName: string, functionArgs: string): Promise<any> {
-    const actualContract = (await this.getRawContract());
-    const argsArray = functionArgs.split(',');  // Split into array based on a delimiter (adjust as necessary)
-    
-    let myFragment: FunctionFragment | undefined = undefined;
+  /**
+ * Tokenizes a string of arguments into an array, handling nested JSON and quotes.
+ * @param input The input string to tokenize.
+ * @returns An array of arguments.
+ */
+private tokenizeArguments(input: string): string[] {
+  const tokens: string[] = [];
+  let currentToken = '';
+  let insideQuotes = false;
+  let bracketDepth = 0;
 
-    const fragments = actualContract.interface.fragments;
-    for(const f of fragments) {
-      if(!ethers.FunctionFragment.isFunction(f)) {
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+
+    if (char === '"' && (i === 0 || input[i - 1] !== '\\')) {
+      insideQuotes = !insideQuotes;
+    } else if (!insideQuotes) {
+      if (char === '{') {
+        bracketDepth++;
+      } else if (char === '}') {
+        bracketDepth--;
+      } else if (char === ',' && bracketDepth === 0) {
+        tokens.push(currentToken.trim());
+        currentToken = '';
         continue;
       }
-      if((f as FunctionFragment).name == functionName) {
+    }
+    currentToken += char;
+  }
+  if (currentToken.trim() !== '') {
+    tokens.push(currentToken.trim());
+  }
+
+  return tokens;
+}
+
+  private async prepareCall(functionName: string, functionArgs: string): Promise<any> {
+    const actualContract = await this.getRawContract();
+    const fragments = actualContract.interface.fragments;
+  
+    // Find the corresponding FunctionFragment
+    let myFragment: FunctionFragment | undefined = undefined;
+    for (const f of fragments) {
+      if (!ethers.FunctionFragment.isFunction(f)) {
+        continue;
+      }
+      if ((f as FunctionFragment).name === functionName) {
         myFragment = f as FunctionFragment;
       }
     }
-
-    if(myFragment == undefined) {
-      throw new Error(`Cannot find ABI-definition of function ${functionName}`)
+  
+    if (myFragment === undefined) {
+      throw new Error(`Cannot find ABI-definition of function ${functionName}`);
     }
-
-    // Step 3: Convert the arguments to the correct types
+  
+    // Regex to split the functionArgs while preserving JSON and quoted strings
+    const argsArray = this.tokenizeArguments(functionArgs);
+  
+    // Convert the arguments to the correct types using ABI coder
     const abiCoder = new AbiCoder();
     const convertedArgs = argsArray.map((arg, index) => {
-      const inputType = myFragment.inputs[index].type;
+      const inputType = myFragment!.inputs[index].type;
+  
+      // Ignore arguments wrapped in '...'
+      if (arg.startsWith("'") && arg.endsWith("'")) {
+        return arg.slice(1, -1); // Strip the quotes and use as-is
+      }
+  
+      // Parse JSON objects
+      try {
+        if (arg.startsWith('{') && arg.endsWith('}')) {
+          return JSON.parse(arg); // JSONs are usually structs - keep them.
+        }
+      } catch {
+        // If JSON parsing fails, fall back to treating as a string/number        
+      }
+  
+      // Treat as a string or number
       return abiCoder.decode([inputType], abiCoder.encode([inputType], [arg]))[0];
     });
-    
-    // FIXME error handling!!
-    return {args: convertedArgs, fragment: myFragment}
+  
+    return { args: convertedArgs, fragment: myFragment };
   }
+  
 
   public async execute(functionName: string, functionArgs: string) {
     const callData = await this.prepareCall(functionName, functionArgs);
     const actualContract = (await this.getRawContract());
 
-    if(callData.fragment.stateMutability != "view") {
-      // Execute the transfer function
-      // FIXME check this with the ABI and  transform args accordingly!!!!!!
+    if(callData.fragment.stateMutability == "nonpayable" || callData.fragment.stateMutability == "payable") {
+      // Execute the transfer function and modify blockchain state
       const tx = await actualContract[functionName](...callData.args);
-
-      // TODO Optional: Wait for the transaction to be mined
       const receipt = await tx.wait();
       return JSON.stringify(receipt);
     }
     else {
-      const returnValue = actualContract[functionName](...callData.args);
+      const returnValue = await actualContract[functionName](...callData.args);
+      // TODO consider a JSON or defined return value here
+      // When using json, think about json-bigint or other packages!
       return returnValue;
     }   
   }
